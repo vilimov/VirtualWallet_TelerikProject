b3e5af44fd525c19164c5b8f92fb47d4d7722a4c
kocip <kocip@FREAKY>
2023-07-20 19:21:03 +0300

    Adding view model folder

--- Virtual Wallet/Virtual Wallet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Virtual_Wallet.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
