199f4f61865a81a1447b442d7dc50410bbc7e262
kocip <kocip@FREAKY>
2023-07-22 20:48:27 +0300

    Adding some User DTO's

---
+++ Virtual Wallet/Virtual Wallet/Models/Dtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Virtual_Wallet.Models.Dtos
+{
+	public class UserLoginDto
+	{
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The {0} field is required and must not be empty.")]
+		[MinLength(2), MaxLength(20)]
+		public string Username { get; set; }
+
+		[Required(AllowEmptyStrings = false, ErrorMessage = "The {0} field is required and must not be empty.")]
+		[MinLength(8), MaxLength(20)]
+		public string Password { get; set; }
+	}
+}
