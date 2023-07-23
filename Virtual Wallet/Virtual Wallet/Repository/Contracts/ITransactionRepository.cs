07f436728c5b7362498d13c17b13f4285691890d
kocip <kocip@FREAKY>
2023-07-23 13:12:13 +0300

    Adding TransactionRepo

--- Virtual Wallet/Virtual Wallet/Repository/Contracts/ITransactionRepository.cs
+++ Virtual Wallet/Virtual Wallet/Repository/Contracts/ITransactionRepository.cs
@@ -1,6 +1,11 @@
 ﻿namespace Virtual_Wallet.Repository.Contracts
 {
-	public interface TransactionRepository
+	public interface ITransactionRepository
 	{
+		Task<Transaction> GetTransactionById(int id);
+		Task<PageResult<Transaction>> GetAllTransactionsForUser(int userId, int pageNumber, int pageSize = 10);
+		Task<Transaction> AddTransaction(Transaction transaction);
+		Task<Transaction> UpdateTransaction(Transaction transaction);
+		Task DeleteTransaction(int id);
 	}
 }
