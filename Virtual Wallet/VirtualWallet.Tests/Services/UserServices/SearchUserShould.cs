1958b774897020d80afeaaf0c423d5ebc6887f75
kocip <kocip@FREAKY>
2023-08-01 18:10:18 +0300

    Adding tests for users

---
+++ Virtual Wallet/VirtualWallet.Tests/Services/UserServices/SearchUserShould.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Virtual_Wallet.VirtualWallet.Application.Services;
+using Virtual_Wallet.VirtualWallet.Domain.Entities;
+using Virtual_Wallet.VirtualWallet.Persistence.Repository.Contracts;
+using VirtualWallet.Application.Services.Contracts;
+using VirtualWallet.Tests.TestHelpers;
+
+namespace VirtualWallet.Tests.Services.UserServices
+{
+	[TestClass]
+	public class SearchUserShould
+	{
+		[TestMethod]
+		public void SearchByUsername_UsernameExists_ReturnsUsers()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+			List<User> testUsers = UsersHelper.GetTestUsersList();
+
+			userRepoMock.Setup(repo => repo.SearchByUsername("ElonMusk")).Returns(testUsers.Where(u => u.Username == "ElonMusk"));
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Act
+			var result = sut.SearchByUsername("ElonMusk");
+
+			// Assert
+			Assert.IsTrue(result.Any());
+			Assert.AreEqual(1, result.Count());
+			Assert.AreEqual("ElonMusk", result.First().Username);
+		}
+
+		[TestMethod]
+		public void SearchByEmail_EmailExists_ReturnsUsers()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+			List<User> testUsers = UsersHelper.GetTestUsersList();
+
+			userRepoMock.Setup(repo => repo.SearchByEmail("elon@musk.com")).Returns(testUsers.Where(u => u.Email == "elon@musk.com"));
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Act
+			var result = sut.SearchByEmail("elon@musk.com");
+
+			// Assert
+			Assert.IsTrue(result.Any());
+			Assert.AreEqual(1, result.Count());
+			Assert.AreEqual("elon@musk.com", result.First().Email);
+		}
+
+		[TestMethod]
+		public void SearchByPhone_PhoneExists_ReturnsUsers()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+			List<User> testUsers = UsersHelper.GetTestUsersList();
+
+			userRepoMock.Setup(repo => repo.SearchByPhone("1234567890")).Returns(testUsers.Where(u => u.PhoneNumber == "1234567890"));
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Act
+			var result = sut.SearchByPhone("1234567890");
+
+			// Assert
+			Assert.IsTrue(result.Any());
+			Assert.AreEqual(1, result.Count());
+			Assert.AreEqual("1234567890", result.First().PhoneNumber);
+		}
+	}
+}
---
+++ Virtual Wallet/VirtualWallet.Tests/Services/UserServices/UpdateUserShould.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Virtual_Wallet.VirtualWallet.Application.Services;
+using Virtual_Wallet.VirtualWallet.Common.Exceptions;
+using Virtual_Wallet.VirtualWallet.Domain.Entities;
+using Virtual_Wallet.VirtualWallet.Persistence.Repository.Contracts;
+using VirtualWallet.Application.Services.Contracts;
+using VirtualWallet.Tests.TestHelpers;
+
+namespace VirtualWallet.Tests.Services.UserServices
+{
+	[TestClass]
+	public class UpdateUserShould
+	{
+		[TestMethod]
+		public void UpdateUser_ValidUser_UpdatesEmailAndPassword()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+
+			User testUser = UsersHelper.GetTestUser();
+			User updateUser = new User
+			{
+				Id = testUser.Id,
+				Email = "newemail@test.com",
+				Password = "NewPassword123!"
+			};
+
+			userRepoMock.Setup(repo => repo.GetUserById(testUser.Id)).Returns(testUser);
+			userRepoMock.Setup(repo => repo.UpdateUser(It.IsAny<User>()));
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Act
+			sut.UpdateUser(updateUser);
+
+			// Assert
+			userRepoMock.Verify(repo => repo.UpdateUser(It.Is<User>(u =>
+	                     u.Email == updateUser.Email && u.Password != It.IsAny<string>())));
+		}
+
+		[TestMethod]
+		public void UpdateUser_InvalidUser_ThrowsEntityNotFoundException()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+
+			User updateUser = new User
+			{
+				Id = 0,
+				Email = "newemail@test.com",
+				Password = "NewPassword123!"
+			};
+
+			userRepoMock.Setup(repo => repo.GetUserById(It.IsAny<int>())).Returns((User)null);
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Assert
+			Assert.ThrowsException<EntityNotFoundException>(() => sut.UpdateUser(updateUser));
+		}
+
+		[TestMethod]
+		public void UpdateUser_InvalidPassword_DoesNotUpdatePassword()
+		{
+			// Arrange
+			var userRepoMock = new Mock<IUserRepository>();
+			var emailServiceMock = new Mock<IEmailService>();
+
+			User testUser = UsersHelper.GetTestUser();
+			User updateUser = new User
+			{
+				Id = testUser.Id,
+				Email = "newemail@test.com",
+				Password = "qwerty"
+			};
+
+			userRepoMock.Setup(repo => repo.GetUserById(testUser.Id)).Returns(testUser);
+			userRepoMock.Setup(repo => repo.UpdateUser(It.IsAny<User>()));
+
+			var sut = new UserService(userRepoMock.Object, emailServiceMock.Object);
+
+			// Act
+			sut.UpdateUser(updateUser);
+
+			// Assert
+			userRepoMock.Verify(repo => repo.UpdateUser(It.Is<User>(u =>
+		                 u.Email == updateUser.Email && u.Password == testUser.Password && u.Salt == testUser.Salt)));
+		}
+	}
+}
