2e21c28650bc3f114aaecdbdad128b2c3c92d186
mila.mitova@gmail.com <d]e=_cW)n[8-JZ>
2023-08-03 11:40:44 +0300

    little add up to session

---
+++ Virtual Wallet/Virtual Wallet/Models/ViewModels/MakeCardTransactionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Virtual_Wallet.VirtualWallet.Domain.Entities;
+
+namespace Virtual_Wallet.Models.ViewModels
+{
+	public class MakeCardTransactionViewModel
+	{
+		User User { get; set; }
+		Card Card { get; set; }
+		decimal Amount { get; set; }
+		string Description { get; set; }
+	}
+}
