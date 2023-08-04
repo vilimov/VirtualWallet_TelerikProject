5d0f7fa3648c4b07407a064843cdb18355544a0f
mila.mitova@gmail.com <d]e=_cW)n[8-JZ>
2023-08-04 12:06:44 +0300

    add pop up for transaction confirmation

---
+++ Virtual Wallet/Virtual Wallet/wwwroot/js/PopUpJS.js
@@ -0,0 +1,25 @@
+﻿
+const customPopup = document.getElementById('customPopup');
+const confirmButton = document.getElementById('confirmButton');
+const cancelButton = document.getElementById('cancelButton');
+
+function showCustomPopup() {
+	customPopup.style.display = 'block';
+}
+
+function hideCustomPopup() {
+	customPopup.style.display = 'none';
+}
+
+confirmButton.addEventListener('click', function () {
+	// Trigger the form submission when "Yes" is clicked
+	document.querySelector('form').submit();
+});
+
+cancelButton.addEventListener('click', hideCustomPopup);
+
+// Add this event listener to your existing submit button
+document.querySelector('form').addEventListener('submit', function (e) {
+	e.preventDefault(); // Prevent the default form submission
+	showCustomPopup();
+});
