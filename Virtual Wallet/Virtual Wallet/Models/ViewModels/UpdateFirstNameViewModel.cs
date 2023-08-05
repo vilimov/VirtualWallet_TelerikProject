16bcf6823f80cace8d311d82d8cb3f5fd01d4ea6
kocip <kocip@FREAKY>
2023-08-05 14:25:26 +0300

    Fixing Annotations in update view models and adding icons

--- Virtual Wallet/Virtual Wallet/Models/ViewModels/UpdateFirstNameViewModel.cs
+++ Virtual Wallet/Virtual Wallet/Models/ViewModels/UpdateFirstNameViewModel.cs
@@ -5,8 +5,8 @@ namespace Virtual_Wallet.Models.ViewModels
     public class UpdateFirstNameViewModel
     {
         public string CurrentFirstName { get; set; }
-        [Required]
-        [MaxLength(32, ErrorMessage = "The {0} field must be less than {1} characters.")]
+        [Required(ErrorMessage = "First name field is required.")]
+		[MaxLength(32, ErrorMessage = "The {0} field must be less than {1} characters.")]
         public string NewFirstName { get; set; }
     }
 }
--- Virtual Wallet/Virtual Wallet/Models/ViewModels/UpdatePasswordViewModel.cs
+++ Virtual Wallet/Virtual Wallet/Models/ViewModels/UpdatePasswordViewModel.cs
@@ -4,19 +4,19 @@ namespace Virtual_Wallet.Models.ViewModels
 {
     public class UpdatePasswordViewModel
     {
-        [Required(ErrorMessage = "Current password is required.")]
+        [Required(ErrorMessage = "Current password field is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Current Password")]
         public string CurrentPassword { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "The {0} field is required and must not be empty.")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "The new password field is required.")]
         [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W).{8,}$", ErrorMessage = "Password needs 8+ chars, a lower and upper case, a digit, and a symbol.")]
         [MaxLength(20, ErrorMessage = "The {0} field must be less than {1} characters.")]
         [DataType(DataType.Password)]
         [Display(Name = "New Password")]
         public string NewPassword { get; set; }
 
-        [Required(ErrorMessage = "Confirm password is required.")]
+        [Required(ErrorMessage = "Confirm password field is required.")]
 		[DataType(DataType.Password)]
 		[Display(Name = "Confirm Password")]
 		[Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
