90cdf01c8a2541e94ab3245bda381a4fb05ee964
milkotomov <el_loco@abv.bg>
2023-08-05 15:17:38 +0300

    Cards, Wallets Index View

--- Virtual Wallet/Virtual Wallet/Helpers/Mappers/MappingProfile.cs
+++ Virtual Wallet/Virtual Wallet/Helpers/Mappers/MappingProfile.cs
@@ -12,10 +12,10 @@ namespace Virtual_Wallet.VirtualWallet.API.Helpers.Mappers
     {
         public MappingProfile()
         {
+            // Card Mapping
             CreateMap<CardAddDto, Card>()
                 .ForMember(dest => dest.ExpirationDate, opts => opts.MapFrom(src => DateTime.ParseExact(src.ExpireDateFormatted, "MMyy", CultureInfo.InvariantCulture)));
             CreateMap<Card, CardAddDto>();
-
             CreateMap<CardShowDto, Card>();
             CreateMap<Card, CardShowDto>();
 
@@ -26,8 +26,6 @@ namespace Virtual_Wallet.VirtualWallet.API.Helpers.Mappers
 			CreateMap<Transaction, MakeCardTransactionViewModel>()
 	                .ForMember(dto => dto.Cards, opt => opt.Ignore());
 			CreateMap<MakeCardTransactionViewModel, Transaction>();
-
-
 
 			//User mappings
 			CreateMap<User, UserShowDto>();
@@ -40,6 +38,7 @@ namespace Virtual_Wallet.VirtualWallet.API.Helpers.Mappers
                 .ForMember(dest => dest.Email, opts => opts.MapFrom(src => src.Email))
                 .ForMember(dest => dest.Password, opts => opts.MapFrom(src => src.Password));
 
+            // Wallet Mapping
             CreateMap<WalletCreateUpdateDto, Wallet>();
         }
     }
